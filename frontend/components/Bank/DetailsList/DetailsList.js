c98c7c88d34f60e2a5a945372dfed75b70647898
Ken Reese <krrgithub@gmail.com>
2017-10-23 15:17:26 -0600

    Header and footer

--- frontend/components/Bank/DetailsList/DetailsList.js
+++ frontend/components/Bank/DetailsList/DetailsList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { accountTypes, accountTypesMap, TAccountList } from "../../../constants";
 import Accounting from "../../../lib/accounting";
+import { Link } from "react-router-dom";
 
 import "./DetailsList.scss";
 
@@ -24,10 +25,24 @@ export default class DetailsList extends React.Component {
         return (
             <tr className="__row" key={reactKey}>
                 <td style={{width: "1em", backgroundColor: color}}></td>
-                <td className="__displayName">{account["displayName"]} (...{this.renderAccountNumber(account["number"])})</td>
+                <td className="__displayName">
+                    {account["displayName"]} (...{this.renderAccountNumber(account["number"])})
+                </td>
                 <td className="__interestYTD">{Accounting.formatMoney(account["interestYTD"])}</td>
                 <td className="__apy">{Accounting.toFixed(account["apy"], 2)}%</td>
                 <td className="__balance">{Accounting.formatMoney(account["balance"])}</td>
+            </tr>
+        )
+    }
+
+    renderTotalRow = (total) => {
+        return (
+            <tr className="TotalRow">
+                <td></td>
+                <td className="__displayName"></td>
+                <td className="__interestYTD"></td>
+                <td className="__apy">Total:</td>
+                <td className="__balance">{Accounting.formatMoney(total)}</td>
             </tr>
         )
     }
@@ -39,18 +54,35 @@ export default class DetailsList extends React.Component {
             return null;
         }
 
-        const typeKey = accountTypeObj["key"]
+        const typeKey = accountTypeObj["key"];
         const reactKey = `${typeKey}-${filteredAccounts.map(a => a["balance"]).join("-")}`;
-        const color = accountTypesMap[typeKey]["color"]
+        const color = accountTypesMap[typeKey]["color"];
+        const totalBalance = _.sumBy(filteredAccounts, a => a["balance"]);
 
         return (
             <div className="__content" key={reactKey}>
-                <h3>{ accountTypeObj["displayName"] }</h3>
+
+
+                <table className="__headerTable">
+                    <thead>
+                        <tr>
+                            <th className="__accountType"><h3>{ accountTypeObj["displayName"] }</h3></th>
+                            <th className="__displayName"></th>
+                            <th className="__interestYTD">Interest YTD</th>
+                            <th className="__apy">APY</th>
+                            <th className="__balance">Available</th>
+                        </tr>
+                    </thead>
+                </table>
 
                 <table>
+
                     <tbody>
                     { filteredAccounts.map(a => this.renderAccountRow(a, color)) }
                     </tbody>
+                    <tfoot>
+                    { this.renderTotalRow(totalBalance) }
+                    </tfoot>
                 </table>
 
             </div>
